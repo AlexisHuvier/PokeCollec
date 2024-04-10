1d9572639cc6bcdffe219b2d3d4f59b898036ae5
Alexis Huvier <lavapower84@gmail.com>
2024-04-10 14:59:35 +0200

    feat(repository): meilleure gestion des erreurs

--- PokeCollec/Repository/PokeRepository.cs
+++ PokeCollec/Repository/PokeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PokeCollec.Model.TCGDex;
+using SharpEngine.Core.Manager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,23 @@ public class PokeRepository
     public static HttpClient Client { get; set; } = new HttpClient();
     public const string BaseUrl = "https://api.tcgdex.net/v2/fr";
 
-    public List<SerieResume>? GetSeries() => Client.GetAsync($"{BaseUrl}/series").Result.Content.ReadFromJsonAsync<List<SerieResume>>().Result!;
-    public List<SetResume>? GetSets() => Client.GetAsync($"{BaseUrl}/sets").Result.Content.ReadFromJsonAsync<List<SetResume>>().Result!;
-    public Serie? GetSerie(string id) => Client.GetAsync($"{BaseUrl}/series/{id}").Result.Content.ReadFromJsonAsync<Serie>().Result!;
-    public Set? GetSet(string id) => Client.GetAsync($"{BaseUrl}/sets/{id}").Result.Content.ReadFromJsonAsync<Set>().Result!;
-    public Card? GetCard(string id) => Client.GetAsync($"{BaseUrl}/cards/{id}").Result.Content.ReadFromJsonAsync<Card>().Result!;
+    public List<SerieResume>? GetSeries() => Call<List<SerieResume>>($"{BaseUrl}/series");
+
+    public List<SetResume>? GetSets() => Call<List<SetResume>>($"{BaseUrl}/sets");
+    public Serie? GetSerie(string id) => Call<Serie>($"{BaseUrl}/series/{id}");
+    public Set? GetSet(string id) => Call<Set>($"{BaseUrl}/sets/{id}");
+    public Card? GetCard(string id) => Call<Card>($"{BaseUrl}/cards/{id}");
+
+    private T? Call<T>(string route)
+    {
+        try
+        {
+            return Client.GetAsync(route).Result.Content.ReadFromJsonAsync<T>().Result;
+        }
+        catch (Exception e)
+        {
+            DebugManager.Log(SharpEngine.Core.Utils.LogLevel.LogDebug, $"Erreur : {e}");
+            return default;
+        }
+    }
 }
