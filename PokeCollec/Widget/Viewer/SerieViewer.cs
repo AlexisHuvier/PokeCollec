ed24f0f257a688862699024f3556988a14f153b2
Alexis Huvier <lavapower84@gmail.com>
2024-04-12 11:54:35 +0200

    feat(serieviewer): finalisation de l'ui

--- PokeCollec/Widget/Viewer/SerieViewer.cs
+++ PokeCollec/Widget/Viewer/SerieViewer.cs
@@ -1,4 +1,5 @@
 ﻿using PokeCollec.Model.TCGDex;
+using SharpEngine.Core.Manager;
 using SharpEngine.Core.Math;
 using SharpEngine.Core.Widget;
 using System;
@@ -11,16 +12,25 @@ namespace PokeCollec.Widget.Viewer;
 
 public class SerieViewer : BaseViewer<Serie>
 {
+    private Label Title { get; }
+    private Image Logo { get; }
     private ListViewer<SetResumeViewer, SetResume> SetsViewer { get; }
 
 
     public SerieViewer(Vec2 position) : base(position, new Vec2(1100, 650))
     {
+        Title = AddChild(new Label(new Vec2(0, -300), "Title", "30"));
+        Logo = AddChild(new Image(new Vec2(0, -225)));
         SetsViewer = AddChild(new ListViewer<SetResumeViewer, SetResume>(new Vec2(0, -175), "Sets", 2, 1));
     }
 
     public override void SetValue(Serie value)
     {
-        SetsViewer.SetValues(value.Sets);
+        Title.Text = $"{value.Name} ({value.Id})";
+        if (value.Logo != null)
+            Logo.Texture = PokeCollec.CacheRepository.Get($"{value.Id}.png", $"{value.Logo}.png");
+        else
+            Logo.Texture = "";
+        SetsViewer.SetValues(value.Sets ?? []);
     }
 }
